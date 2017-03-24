ef73fdbb25f113f801339a40473887eba16fa0bc
SooLee <duplexa@gmail.com>
2017-03-23 20:06:45 -0400

    updated test for pairsqc based on du

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,14 +6,22 @@ image_name=docker-4dn-hic_local  # local image name for testing
 cwd=$(pwd)
 mkdir -p tmp_out
 docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name run-cooler.sh /d/tst.gz /d/hg19.chrom.sizes.mainonly 100000 5000 2 /e/tst
-cooler dump --join tmp_out/tst.cool > tmp_out/tst.cooldump
-diff tests/tst.cooldump tmp_out/tst.cooldump
+docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name cooler dump --join /e/tst.cool > tmp_out/tst.cooldump
+if [ ! -z "$(diff tests/tst.cooldump tmp_out/tst.cooldump)" ]; then
+  return 1;
+fi
 
 
 ## run-pairsqc-single test
 docker run -it -v $(pwd)/sample_data/:/sample_data/:ro -v $(pwd)/tmp_out/:/out/:rw $image_name run-pairsqc-single.sh /sample_data/tst.gz /sample_data/hg19.chrom.sizes.mainonly tst 4 /out/tst
 cd tmp_out/tst/
 unzip -o tst_report.zip
-ls -lhtr tst_report
 cd ../../
+du -s tests/pairsqc/tst_report/ |cut -f1 > pairsqc.log1
+du -s tmp_out/tst/tst_report/ | cut -f1 > pairsqc.log2
+if [ ! -z "$(diff pairsqc.log1 pairsqc.log2)" ]; then
+  return 1;
+fi
+
+return 0;
 
