a2f16db8ec7adf8d8a4c9ffca9ee6ce4dfb21cc9
SooLee <duplexa@gmail.com>
2017-03-29 23:07:28 -0400

    using a larger test file for cooler test, (so cooler balance does not fail sometimes)

--- tests/tests.sh
+++ tests/tests.sh
@@ -5,9 +5,9 @@ image_name=docker-4dn-hic_local  # local image name for testing
 ## run-cooler test
 cwd=$(pwd)
 mkdir -p tmp_out
-docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name run-cooler.sh /d/tst.gz /d/hg19.chrom.sizes.mainonly 100000 5000 2 /e/tst
-docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name cooler dump --join /e/tst.cool > tmp_out/tst.cooldump
-if [ ! -z "$(diff tests/tst.cooldump tmp_out/tst.cooldump)" ]; then
+docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name run-cooler.sh /d/test.pairs.gz /d/hg19.chrom.sizes.mainonly 100000 5000 2 /e/test
+docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name cooler dump --join /e/test.cool > tmp_out/test.cooldump
+if [ ! -z "$(diff tests/test.cooldump tmp_out/test.cooldump)" ]; then
   return 1;
 fi
 
