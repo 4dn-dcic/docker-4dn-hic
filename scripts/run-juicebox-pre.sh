04a5659101e01d7d4189bd4ed2fad0fb90c5e9ab
SooLee <duplexa@gmail.com>
2017-05-24 18:43:37 -0400

    fixed run-juicebox-pre

--- scripts/run-juicebox-pre.sh
+++ scripts/run-juicebox-pre.sh
@@ -6,15 +6,15 @@ min_res=$4   # e.g. 5000
 higlass=$5   # if 1, higlass-compatible aggregation
 
 # creating a hic file
-java -jar juicer_tools  pre -n $input_pairs $output_prefix.hic $chromsizefile
-
-# normalization
-if (( $higlass == 1 ))
+if [[ $higlass == '1' ]]
 then
     reslist=$(python3 -c "from cooler.contrib import higlass; higlass.print_zoom_resolutions('$chromsizefile', $min_res)")
-    java -jar juicer_tools addNorm -w $reslist -d -F $output_prefix.hic
-elif
-then
-    java -jar juicer_tools addNorm -w $min_res -d -F $output_prefix.hic
+    java -jar juicer_tools pre -n $input_pairs $output_prefix.hic $chromsizefile -r $reslist
+else
+    java -jar juicer_tools pre -n $input_pairs $output_prefix.hic $chromsizefile
 fi
+
+# normalization
+java -jar juicer_tools addNorm -w $min_res -d -F $output_prefix.hic
+
 
--- tests/tests.sh
+++ tests/tests.sh
@@ -13,10 +13,8 @@ fi
 
 ## run-cool2multirescool test
 docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name run-cool2multirescool.sh /d/test.cool 2 /e/test
-du -s tmp_out/test.multires.cool |cut -f1 > mcool.log1
-du -s tests/test.multires.cool |cut -f1 > mcool.log2
-docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name python3 -c 'import cooler; print(cooler.io.ls("tmp_out/test.multires.cool"))' >> mcool.log1
-docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name python3 -c 'import cooler; print(cooler.io.ls("tests/test.multires.cool"))' >> mcool.log2
+docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name python3 -c 'import cooler; print(cooler.io.ls("/e/test.multires.cool"))' > mcool.log1
+docker run -it -v $cwd/tests/:/f/:ro -v $cwd/tmp_out/:/e/:rw $image_name python3 -c 'import cooler; print(cooler.io.ls("/f/test.multires.cool"))' > mcool.log2
 if [ ! -z "$(diff mcool.log1 mcool.log2)" ]; then
   return 1;
 fi
@@ -36,7 +34,7 @@ fi
 
 ## run-addfrag2pairs & run-juicebox-pre test
 docker run -it -v $(pwd)/sample_data/:/sample_data/:ro -v $(pwd)/tmp_out/:/out/:rw $image_name run-addfrag2pairs.sh /sample_data/test2.pairs.gz /sample_data/hg19_DpnII.mainonly.txt /out/test
-docker run -it -v $(pwd)/sample_data/:/sample_data/:ro -v $(pwd)/tmp_out/:/out/:rw $image_name run-juicebox-pre.sh /out/test.ff.pairs.gz /sample_data/hg19.chrom.sizes.mainonly /out/test 5000 30 0
+docker run -it -v $(pwd)/sample_data/:/sample_data/:ro -v $(pwd)/tmp_out/:/out/:rw $image_name run-juicebox-pre.sh /out/test.ff.pairs.gz /sample_data/hg19.chrom.sizes.mainonly /out/test 5000 0
 docker run -it -v $(pwd)/sample_data/:/sample_data/:ro -v $(pwd)/tmp_out/:/out/:rw $image_name java -jar juicer_tools dump observed NONE /out/test.hic chr1 chr1 BP 5000 > tmp_out/test.hicdump
 gunzip -c tmp_out/test.ff.pairs.gz | grep -v "^#" | awk '$2=="chr1" && $4=="chr1" && $8!=$9' |wc -l | sed 's/ //g' > juicebox-pre.log1
 cut -f3 tmp_out/test.hicdump | tail -n +4 | perl -ne 'chomp; $s+=$_; print "$s\n";' - |tail -1 > juicebox-pre.log2
@@ -51,7 +49,7 @@ if [ ! -z "$(diff juicebox-norm.log1 juicebox-norm.log2)" ]; then
 fi
 
 # run-juicebox-pre test with higlass-compatible zoom levels 
-docker run -it -v $(pwd)/sample_data/:/sample_data/:ro -v $(pwd)/tmp_out/:/out/:rw $image_name run-juicebox-pre.sh /out/test.ff.pairs.gz /sample_data/hg19.chrom.sizes.mainonly /out/test 5000 30 1
+docker run -it -v $(pwd)/sample_data/:/sample_data/:ro -v $(pwd)/tmp_out/:/out/:rw $image_name run-juicebox-pre.sh /out/test.ff.pairs.gz /sample_data/hg19.chrom.sizes.mainonly /out/test 5000 1
 
 
 return 0;
