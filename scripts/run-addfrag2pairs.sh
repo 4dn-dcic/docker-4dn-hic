843874a11838ee9f18a61f84577681217b815a89
SooLee <duplexa@gmail.com>
2017-07-02 16:52:00 -0400

    updated run-addfrag2pairs.sh so it can deal with pairs files with existing frag fields.

--- scripts/run-addfrag2pairs.sh
+++ scripts/run-addfrag2pairs.sh
@@ -2,6 +2,6 @@
 input_pairs=$1
 restriction_file=$2  # Juicer format
 output_prefix=$3
-gunzip -c $input_pairs | fragment_4dnpairs.pl - $output_prefix.ff.pairs $restriction_file
+gunzip -c $input_pairs | pairix/util/fragment_4dnpairs.pl -a - $output_prefix.ff.pairs $restriction_file
 bgzip -f $output_prefix.ff.pairs
 pairix -f $output_prefix.ff.pairs.gz
