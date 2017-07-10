26959e66ad81f218db03fa9a3ed364028e2c7823
SooLee <duplexa@gmail.com>
2017-07-09 21:06:33 -0400

    added run-mcool2hic.sh

---
+++ scripts/run-mcool2hic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+input_mcool=$1
+outdir=$2
+output_prefix=$3
+nres=$4
+
+python3 get_bins_from_mcool.py -i $input_mcool -o $outdir/$output_prefix.mcool.bins -n $nres
+./convert_mcoolbins_to_juicer_format.pl $outdir/$output_prefix.mcool.bins $outdir/$output_prefix.mcool.bins.juicerformat
+cd $outdir
+gzip $output_prefix.mcool.bins.juicerformat
+
