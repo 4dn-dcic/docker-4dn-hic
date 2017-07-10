44498c4d4e5e5dd941dd75c69e17bc471b94c6a9
SooLee <duplexa@gmail.com>
2017-07-10 12:24:30 -0400

    updated run-mcool2hic

--- scripts/run-mcool2hic.sh
+++ scripts/run-mcool2hic.sh
@@ -5,8 +5,9 @@ outdir=$2
 output_prefix=$3
 nres=$4
 
-python3 get_bins_from_mcool.py -i $input_mcool -o $outdir/$output_prefix.mcool.bins -n $nres
-./convert_mcoolbins_to_juicer_format.pl $outdir/$output_prefix.mcool.bins $outdir/$output_prefix.mcool.normvector.juicerformat
+scriptdir=/usr/local/bin
+python3 $scriptdir/mcool2hic/get_bins_from_mcool.py -i $input_mcool -o $outdir/$output_prefix.mcool.bins -n $nres
+$scriptdir/mcool2hic/convert_mcoolbins_to_juicer_format.pl $outdir/$output_prefix.mcool.bins $outdir/$output_prefix.mcool.normvector.juicerformat
 cd $outdir
 gzip $output_prefix.mcool.normvector.juicerformat
 
