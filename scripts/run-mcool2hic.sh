98c417a487d7f035c12d1eba2a823d0edb7b6b74
SooLee <duplexa@gmail.com>
2017-07-10 10:58:05 -0400

    updated file format for run-mcool2hic

--- scripts/run-mcool2hic.sh
+++ scripts/run-mcool2hic.sh
@@ -6,7 +6,7 @@ output_prefix=$3
 nres=$4
 
 python3 get_bins_from_mcool.py -i $input_mcool -o $outdir/$output_prefix.mcool.bins -n $nres
-./convert_mcoolbins_to_juicer_format.pl $outdir/$output_prefix.mcool.bins $outdir/$output_prefix.mcool.bins.juicerformat
+./convert_mcoolbins_to_juicer_format.pl $outdir/$output_prefix.mcool.bins $outdir/$output_prefix.mcool.normvector.juicerformat
 cd $outdir
-gzip $output_prefix.mcool.bins.juicerformat
+gzip $output_prefix.mcool.normvector.juicerformat
 
