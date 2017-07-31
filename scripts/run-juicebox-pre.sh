56399f5fa4b6afe555e6f3ea8fd7b659b94e1bea
SooLee <duplexa@gmail.com>
2017-07-31 10:52:41 -0400

    added java maxmem as argument in run-juicebox-pre.sh

--- scripts/run-juicebox-pre.sh
+++ scripts/run-juicebox-pre.sh
@@ -4,17 +4,17 @@ chromsizefile=$2
 output_prefix=$3
 min_res=$4   # e.g. 5000
 higlass=$5   # if 1, higlass-compatible aggregation
+maxmem=$6   # e.g. 64g
 
 # creating a hic file
 if [[ $higlass == '1' ]]
 then
     reslist=$(python3 -c "from cooler.contrib import higlass; higlass.print_zoom_resolutions('$chromsizefile', $min_res)")
-    java -jar /usr/local/bin/juicer_tools.jar pre -n $input_pairs $output_prefix.hic $chromsizefile -r $reslist
+    java -Xmx$maxmem -Xms2g -jar /usr/local/bin/juicer_tools.jar pre -n $input_pairs $output_prefix.hic $chromsizefile -r $reslist
 else
-    java -jar /usr/local/bin/juicer_tools.jar pre -n $input_pairs $output_prefix.hic $chromsizefile
+    java -Xmx$maxmem -Xms2g -jar /usr/local/bin/juicer_tools.jar pre -n $input_pairs $output_prefix.hic $chromsizefile
 fi
 
 # normalization
-java -jar /usr/local/bin/juicer_tools.jar addNorm -w $min_res -d -F $output_prefix.hic
-
+java -Xmx$maxmem -Xms2g -jar /usr/local/bin/juicer_tools.jar addNorm -w $min_res -d -F $outpu
 
