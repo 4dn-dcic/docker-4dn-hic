b5385af98da0f161a3356a7f2ed2615e53c51347
SooLee <duplexa@gmail.com>
2017-08-01 16:27:00 -0400

    fixed type in run-juicebox-pre

--- scripts/run-juicebox-pre.sh
+++ scripts/run-juicebox-pre.sh
@@ -16,5 +16,5 @@ else
 fi
 
 # normalization
-java -Xmx$maxmem -Xms2g -jar /usr/local/bin/juicer_tools.jar addNorm -w $min_res -d -F $output
+java -Xmx$maxmem -Xms2g -jar /usr/local/bin/juicer_tools.jar addNorm -w $min_res -d -F $output_prefix.hic
 
