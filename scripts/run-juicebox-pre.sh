974ddf0156aa53c118602f4543813010b428ce2c
SooLee <duplexa@gmail.com>
2017-08-02 17:16:55 -0400

    run-juicebox-pre.sh: equal Xmx Xms now

--- scripts/run-juicebox-pre.sh
+++ scripts/run-juicebox-pre.sh
@@ -10,12 +10,12 @@ maxmem=$6   # e.g. 64g
 if [[ $higlass == '1' ]]
 then
     reslist=$(python3 -c "from cooler.contrib import higlass; higlass.print_zoom_resolutions('$chromsizefile', $min_res)")
-    java -Xmx$maxmem -Xms2g -jar /usr/local/bin/juicer_tools.jar pre -n $input_pairs $output_prefix.hic $chromsizefile -r $reslist
+    java -Xmx$maxmem -Xms$maxmem -jar /usr/local/bin/juicer_tools.jar pre -n $input_pairs $output_prefix.hic $chromsizefile -r $reslist
 else
-    java -Xmx$maxmem -Xms2g -jar /usr/local/bin/juicer_tools.jar pre -n $input_pairs $output_prefix.hic $chromsizefile
+    java -Xmx$maxmem -Xms$maxmem -jar /usr/local/bin/juicer_tools.jar pre -n $input_pairs $output_prefix.hic $chromsizefile
 fi
 
 # normalization
-java -Xmx$maxmem -Xms2g -jar /usr/local/bin/juicer_tools.jar addNorm -w $min_res -d -F $output_prefix.hic
+java -Xmx$maxmem -Xms$maxmem -jar /usr/local/bin/juicer_tools.jar addNorm -w $min_res -d -F $output_prefix.hic
 
 
