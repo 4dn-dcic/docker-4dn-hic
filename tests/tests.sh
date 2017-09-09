13a00765f7e7c8a20c29bb0916d4688601045605
SooLee <duplexa@gmail.com>
2017-09-09 15:00:27 -0400

    changed outdir for tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,5 +1,5 @@
 CWL_RUNNER=$(which cwl-runner)
 CWD=$(pwd)
-mkdir -p $CWD/tmp_out
+OUDDIR=$CWD/tests/test_outdir
 cd cwl_awsem
-$CWL_RUNNER --outdir=$CWD/tmp_out md5.cwl ../tests/test_input_json/md5_input.json
+$CWL_RUNNER --outdir=$OUTDIR md5.cwl ../tests/test_input_json/md5_input.json
