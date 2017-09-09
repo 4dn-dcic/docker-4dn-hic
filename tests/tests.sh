10a9bebb36f594b1ab2f8165dff32c2a4ef4f2a9
SooLee <duplexa@gmail.com>
2017-09-09 14:50:46 -0400

    cwl-runner path

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,3 +1,4 @@
+CWL_RUNNER=$(which cwl-runner)
 mkdir -p tmp_out
 cd cwl_awsem
-cwl-runner --outdir=../tmp_out md5.cwl ../tests/test_input_json/md5_input.json
+$CWL_RUNNER --outdir=../tmp_out md5.cwl ../tests/test_input_json/md5_input.json
