0ceadb553f4877b42f8bff2d74cf49e92d00f454
SooLee <duplexa@gmail.com>
2017-09-09 16:26:49 -0400

    updated test runner flag

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,7 +3,8 @@ CWL_FILENAME=$1.cwl
 INPUT_JSON_NAME=$1.input.json
 CWD=$(pwd)
 OUTDIR=$CWD/tests/test_outdir
+LOCAL_CWL_TMPDIR=$CWD/tests/test_tmp
 CWL_RUNNER=$(which cwltool)
-RUNNER_FLAGS="--no-read-only --outdir=$OUTDIR"
+RUNNER_FLAGS="--copy-outputs --no-read-only --no-match-user --outdir $OUTDIR --tmp-outdir-prefix $LOCAL_CWL_TMPDIR --tmpdir-prefix $LOCAL_CWL_TMPDIR"
 cd cwl_awsem
 $CWL_RUNNER $RUNNER_FLAGS $CWL_FILENAME ../tests/test_input_json/$INPUT_JSON_NAME | python $CWD/tests/json_null_test.py
