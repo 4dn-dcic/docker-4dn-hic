d68067683f81ccd1f6a62bf9f3b1003c45fd0814
SooLee <duplexa@gmail.com>
2017-09-09 15:33:52 -0400

    added cool2mcool test

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,3 +6,4 @@ cd cwl_awsem
 $CWL_RUNNER --outdir=$OUTDIR md5.cwl ../tests/test_input_json/md5_input.json
 $CWL_RUNNER --outdir=$OUTDIR fastqc-0-11-4-1.cwl ../tests/test_input_json/fastqc_input.json
 $CWL_RUNNER --outdir=$OUTDIR hi-c-processing-partb.cwl ../tests/test_input_json/hi-c-processing-partb.input.json
+$CWL_RUNNER --outdir=$OUTDIR cool2mcool.cwl ../tests/test_input_json/cool2mcool.input.json
