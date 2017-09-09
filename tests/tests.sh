f72242607fb44172ece4c7f30128901370b6ce73
SooLee <duplexa@gmail.com>
2017-09-09 15:25:49 -0400

    added partb to test

--- tests/tests.sh
+++ tests/tests.sh
@@ -5,3 +5,4 @@ OUTDIR=$CWD/tests/test_outdir
 cd cwl_awsem
 $CWL_RUNNER --outdir=$OUTDIR md5.cwl ../tests/test_input_json/md5_input.json
 $CWL_RUNNER --outdir=$OUTDIR fastqc-0-11-4-1.cwl ../tests/test_input_json/fastqc_input.json
+$CWL_RUNNER --outdir=$OUTDIR hi-c-processing-partb.cwl ../tests/test_input_json/hi-c-processing-partb.input.json
