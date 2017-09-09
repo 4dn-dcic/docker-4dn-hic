de3f21c2b755f5e766aec37402785d6c85a09966
SooLee <duplexa@gmail.com>
2017-09-09 15:14:33 -0400

    added fastqc to tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -4,3 +4,4 @@ CWD=$(pwd)
 OUTDIR=$CWD/tests/test_outdir
 cd cwl_awsem
 $CWL_RUNNER --outdir=$OUTDIR md5.cwl ../tests/test_input_json/md5_input.json
+$CWL_RUNNER --outdir=$OUTDIR fastqc-0-11-4-1.cwl ../tests/test_input_json/fastqc_input.json
