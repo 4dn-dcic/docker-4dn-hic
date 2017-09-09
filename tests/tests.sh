ab9a53bfc8e1d9316a0a0217e1fbffd8170f384e
SooLee <duplexa@gmail.com>
2017-09-09 15:41:57 -0400

    each test separately?

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,9 +1,8 @@
+CWL_FILENAME=$1
+INPUT_JSON_NAME=$2
 CWD=$(pwd)
 OUTDIR=$CWD/tests/test_outdir
 CWL_RUNNER=$(which cwltool)
 RUNNER_FLAGS="--no-read-only --outdir=$OUTDIR"
 cd cwl_awsem
-$CWL_RUNNER $RUNNER_FLAGS md5.cwl ../tests/test_input_json/md5_input.json
-$CWL_RUNNER $RUNNER_FLAGS fastqc-0-11-4-1.cwl ../tests/test_input_json/fastqc_input.json
-$CWL_RUNNER $RUNNER_FLAGS hi-c-processing-partb.cwl ../tests/test_input_json/hi-c-processing-partb.input.json
-$CWL_RUNNER $RUNNER_FLAGS cool2mcool.cwl ../tests/test_input_json/cool2mcool.input.json
+$CWL_RUNNER $RUNNER_FLAGS $CWL_FILENAME ../tests/test_input_json/$INPUT_JSON_NAME
---
+++ tests/tests_all.sh
@@ -0,0 +1,9 @@
+CWD=$(pwd)
+OUTDIR=$CWD/tests/test_outdir
+CWL_RUNNER=$(which cwltool)
+RUNNER_FLAGS="--no-read-only --outdir=$OUTDIR"
+cd cwl_awsem
+$CWL_RUNNER $RUNNER_FLAGS md5.cwl ../tests/test_input_json/md5_input.json
+$CWL_RUNNER $RUNNER_FLAGS fastqc-0-11-4-1.cwl ../tests/test_input_json/fastqc_input.json
+$CWL_RUNNER $RUNNER_FLAGS hi-c-processing-partb.cwl ../tests/test_input_json/hi-c-processing-partb.input.json
+$CWL_RUNNER $RUNNER_FLAGS cool2mcool.cwl ../tests/test_input_json/cool2mcool.input.json
