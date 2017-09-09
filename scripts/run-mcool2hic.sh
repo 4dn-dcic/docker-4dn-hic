d54f414b8aee47cc1cf568e659017569be61164c
SooLee <duplexa@gmail.com>
2017-09-09 19:26:22 -0400

    copy input mcool to outdir to avoid readonly problem in mcool2hic

--- scripts/run-mcool2hic.sh
+++ scripts/run-mcool2hic.sh
@@ -5,8 +5,9 @@ outdir=$2
 output_prefix=$3
 nres=$4
 
+cp $input_mcool $outdir/$output_prefix.mcool
 scriptdir=/usr/local/bin
-python3 $scriptdir/mcool2hic/get_bins_from_mcool.py -i $input_mcool -o $outdir/$output_prefix.mcool.bins -n $nres
+python3 $scriptdir/mcool2hic/get_bins_from_mcool.py -i $outdir/output_prefix.mcool -o $outdir/$output_prefix.mcool.bins -n $nres
 $scriptdir/mcool2hic/convert_mcoolbins_to_juicer_format.pl $outdir/$output_prefix.mcool.bins $outdir/$output_prefix.mcool.normvector.juicerformat
 cd $outdir
 gzip $output_prefix.mcool.normvector.juicerformat
