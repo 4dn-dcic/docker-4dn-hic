9829a92998b9db6fc5b0a1b3f67b6a37109f519a
SooLee <duplexa@gmail.com>
2017-09-10 16:11:11 -0400

    typo

--- scripts/run-mcool2hic.sh
+++ scripts/run-mcool2hic.sh
@@ -13,7 +13,7 @@ fi
 
 cp $input_mcool $outdir/$output_prefix.mcool
 scriptdir=/usr/local/bin
-res_list = $(python3 -c "from cooler.contrib import higlass; higlass.print_zoom_resolutions('$chromsizefile', $min_res)")
+res_list=$(python3 -c "from cooler.contrib import higlass; higlass.print_zoom_resolutions('$chromsizefile', $min_res)")
 python3 $scriptdir/mcool2hic/get_bins_from_mcool.py -i $outdir/$output_prefix.mcool -o $outdir/$output_prefix.mcool.bins -n $nres
 $scriptdir/mcool2hic/convert_mcoolbins_to_juicer_format.pl $outdir/$output_prefix.mcool.bins $res_list $outdir/$output_prefix.mcool.normvector.juicerformat
 cd $outdir
