af26964dee60e877dc3cf500726266fee97562cb
SooLee <duplexa@gmail.com>
2017-09-11 15:06:49 -0400

    updated benchmark test

--- tests/tests_benchmark.py
+++ tests/tests_benchmark.py
@@ -4,11 +4,13 @@ from Benchmark import Benchmark as B
 class TestGetOptimalInstanceType(unittest.TestCase):
     def test_get_optimal_instance_type1(self):
         res = B.get_optimal_instance_type()
-        print(res)
+        assert 'instance_type' in res
+        assert res['instance_type'] == 't2.nano'
 
     def test_get_optimal_instance_type2(self):
         res = B.get_optimal_instance_type(cpu=32, mem=16)
-        print(res)
+        assert 'instance_type' in res
+        assert res['instance_type'] == 'c4.8xlarge'
 
 if __name__ == '__main__':
     unittest.main()
