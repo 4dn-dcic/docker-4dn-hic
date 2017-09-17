9d02917d11bf1576b612099a913bd16471e2a3e4
EC2 Default User <ec2-user@ip-172-31-7-171.ec2.internal>
2017-09-17 14:25:04 +0000

    revised run-pairs-patch

--- scripts/run-pairs-patch.sh
+++ scripts/run-pairs-patch.sh
@@ -2,7 +2,7 @@
 
 input_pairs=$1
 output_prefix=$2
-gunzip -c $input_pairs | util/duplicate_header_remover.pl | util/column_remover.pl - sam1 sam2 | util/column_remover.pl -d - 8 | bgzip -c > $outprefix.pairs.gz
+gunzip -c $input_pairs | duplicate_header_remover.pl | column_remover.pl - sam1 sam2 | column_remover.pl -d - 8 | bgzip -c > $outprefix.pairs.gz
 pairix -f $outprefix.pairs.gz
 
 
