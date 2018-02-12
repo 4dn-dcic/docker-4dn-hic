734d80f0c4d7280400ef1d6faedd089cca5ab054
SooLee <duplexa@gmail.com>
2018-02-12 14:21:47 -0500

    minor fix in test cwl

--- tests/tests_cwl.sh
+++ tests/tests_cwl.sh
@@ -6,6 +6,6 @@ OUTDIR=$CWD/tests/test_outdir
 LOCAL_CWL_TMPDIR=$CWD/tests/test_tmp
 CWL_RUNNER=$(which cwltool)
 RUNNER_FLAGS="--copy-outputs --no-read-only --no-match-user --outdir $OUTDIR --tmp-outdir-prefix $LOCAL_CWL_TMPDIR --tmpdir-prefix $LOCAL_CWL_TMPDIR --non-strict --debug"
-cd cwl_awsem
+cd cwl
 $CWL_RUNNER $RUNNER_FLAGS $CWL_FILENAME ../tests/test_input_json/$INPUT_JSON_NAME | python $CWD/tests/json_null_test.py
 cd ..
