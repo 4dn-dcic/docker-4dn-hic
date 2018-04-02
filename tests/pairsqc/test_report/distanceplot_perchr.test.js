01a68f1e7dc2ad5f9c71387ed2d3d841a5645842
EC2 Default User <ec2-user@ip-172-31-48-253.ec2.internal>
2018-04-02 20:07:27 +0000

    working tests for hicprocessingpairs,nore,nonorm

--- tests/pairsqc/test_report/distanceplot_perchr.test.js
+++
@@ -1,3 +0,0 @@
-d3.tsv('./tsvcol.d3div_contact_frequency_vs_genomic_separation_per_chr_test___.tsv', function(tsvcolumns) {
-interactive_multiline_plot('./test.plot_table.out', tsvcolumns, 3.5,7,-13.08,-8.779, 'log10 genomic separation', 'log10 contact frequency', 'd3div_contact_frequency_vs_genomic_separation_per_chr_test___');
-});
