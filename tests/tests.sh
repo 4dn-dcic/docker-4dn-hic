fbf69ef8d4339201d08a4481e9a4dbb657cf612b
SooLee <duplexa@gmail.com>
2018-06-06 19:47:13 +0000

    updated tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+#set -e
 
 image_name=docker-4dn-hic_local  # local image name for testing
 
@@ -15,15 +15,17 @@ if [ ! -z "$(diff tests/test_files/test.cooldump tmp_out/test.cooldump)" ]; then
 fi
 
 ## run-cool2multirescool test
+echo "testing run-cool2multirescool..."
 docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name run-cool2multirescool.sh -i /d/test.cool -p 2 -o /e/test
 docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name python3 -c 'import cooler; print(cooler.io.ls("/e/test.multires.cool"))' > mcool.log1
-docker run -it -v $cwd/tests/:/f/:ro -v $cwd/tmp_out/:/e/:rw $image_name python3 -c 'import cooler; print(cooler.io.ls("/f/test.multires.cool"))' > mcool.log2
+docker run -it -v $cwd/tests/test_files/:/f/:ro -v $cwd/tmp_out/:/e/:rw $image_name python3 -c 'import cooler; print(cooler.io.ls("/f/test.multires.cool"))' > mcool.log2
 if [ ! -z "$(diff mcool.log1 mcool.log2)" ]; then
   echo "zoomify test failed"
   return 1;
 fi
 
 # run-cool2multirescool custom res test
+echo "testing run-cool2multirescool with custom resolutions..."
 docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name run-cool2multirescool.sh -i /d/test.cool -p 2 -o /e/test -u 100000,200000,500000
 docker run -it -v $cwd/sample_data/:/d/:ro -v $cwd/tmp_out/:/e/:rw $image_name python3 -c 'import cooler; print(cooler.io.ls("/e/test.multires.cool"))' > mcool.log1
 if [ ! -z "$(diff mcool.log1 $cwd/tests/test_files/test.multires.cool.customres.log)"]; then
@@ -67,5 +69,5 @@ docker run -it -v $(pwd)/sample_data/:/sample_data/:ro -v $(pwd)/tmp_out/:/out/:
 # run-juicebox-pre test with higlass-compatible zoom levels and normlization only
 docker run -it -v $(pwd)/sample_data/:/sample_data/:ro -v $(pwd)/tmp_out/:/out/:rw $image_name run-juicebox-pre.sh -i /out/test.ff.pairs.gz -c /sample_data/hg19.chrom.sizes.mainonly -o /out/test -r 5000 -g -m 4g -n 
 
-
+set +e
 return 0;
