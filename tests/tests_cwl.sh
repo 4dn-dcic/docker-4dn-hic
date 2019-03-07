bd49797beece1403fd0923dfc0aa37801cd4ff83
Soo Lee <duplexa@gmail.com>
2019-03-07 17:12:26 -0500

    Update tests_cwl.sh

--- tests/tests_cwl.sh
+++ tests/tests_cwl.sh
@@ -1,4 +1,3 @@
-set -e
 CWL_NAME=$1
 CWL_FILENAME=$1.cwl
 INPUT_JSON_NAME=$1.input.json
@@ -6,7 +5,8 @@ CWD=$(pwd)
 OUTDIR=$CWD/tests/test_outdir
 LOCAL_CWL_TMPDIR=$CWD/tests/test_tmp
 CWL_RUNNER=$(which cwltool)
+CWL_RUNNER='python -m cwltool'
 RUNNER_FLAGS="--copy-outputs --no-read-only --no-match-user --outdir $OUTDIR --tmp-outdir-prefix $LOCAL_CWL_TMPDIR --tmpdir-prefix $LOCAL_CWL_TMPDIR --non-strict --debug"
 cd cwl
-$CWL_RUNNER $RUNNER_FLAGS $CWL_FILENAME ../tests/test_input_json/$INPUT_JSON_NAME | python $CWD/tests/json_null_test.py
+$CWL_RUNNER $RUNNER_FLAGS $CWL_FILENAME ../tests/test_input_json/$INPUT_JSON_NAME || exit
 cd ..
